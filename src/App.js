fdeb755edac3ec606aadc5b1f6ccf66c64155e37
Hamza Nadir <hamzanadir.dev@gmail.com>
2023-12-14 16:46:27 +0100

    correction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
-function App() {
+function App(props) {
+
+  const [face, setFace] = useState(null);
+  const [compteur, setCompteur] = useState(0);
+  const [resultat, setResultat] = useState(false);
+  const cachee = props.cachee;
+
+  function jouer(){
+    let random = Math.floor(Math.random()*6+1);
+    if(random == cachee){
+      setFace(random)
+      setCompteur(compteur+1);
+      setResultat(true)
+    }else{
+      setFace(random)
+      setCompteur(compteur+1);
+    }
+  }
+
+  function initialiser() {
+    setFace(null);
+    setCompteur(0);
+    setResultat(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Jeux de DE</h1>
+      <h2>Face : {face}</h2>
+      <h2>Nombre d'essais : {compteur}</h2>
+      {
+        resultat  ? <div><p>Bravo vous avez trouvez la face cachee</p><button onClick={initialiser}>Initialiser</button></div>
+                  : <button onClick={jouer}>jouer</button>
+      }
     </div>
   );
 }
